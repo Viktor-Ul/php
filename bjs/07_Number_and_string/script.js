ded75c41f965b44ed52e57697f1f3c6515afed6a
viktorlnvrsk@gamil.com <viktorlnvrsk@gmail.com>
2025-03-12 16:07:42 +0300

    updating files

--- bjs/07_Number_and_string/script.js
+++ bjs/07_Number_and_string/script.js
@@ -1,12 +1,78 @@
 let lastOperand = 0;
 let operation = null;
 
-const inputWindow = document.getElementById('inputWindow');
+const inputWindow = document.querySelector('#inputWindow');
 
 
-document.getElementById('btn_clr').addEventListener('click', function () {
+document.querySelector('#btn_clr').addEventListener('click', function () {
     lastOperand = 0;
     operation = null;
     inputWindow.value = '';
 })
 
+document.querySelector('#btn_sum').addEventListener('click', function () {
+    lastOperand = parseInt(inputWindow.value);
+    operation = 'sum';
+    inputWindow.value = '';
+})
+
+document.querySelector('#btn_def').addEventListener('click', function () {
+    lastOperand = parseInt(inputWindow.value);
+    operation = 'def';
+    inputWindow.value = '';
+})
+
+document.querySelector('#btn_calc').addEventListener('click', function () {
+    if (operation === 'sum'){
+        const result = lastOperand + parseInt(inputWindow.value)
+        operation = null;
+        lastOperand = 0;
+        inputWindow.value = result;
+    }
+    if (operation === 'def'){
+        const result = lastOperand - parseInt(inputWindow.value)
+        operation = null;
+        lastOperand = 0;
+        inputWindow.value = result;
+    }
+})
+
+document.querySelector('#btn_1').addEventListener('click', function () {
+    inputWindow.value += '1';
+})
+
+document.querySelector('#btn_2').addEventListener('click', function () {
+    inputWindow.value += '2';
+})
+
+document.querySelector('#btn_3').addEventListener('click', function () {
+    inputWindow.value += '3';
+})
+
+document.querySelector('#btn_4').addEventListener('click', function () {
+    inputWindow.value += '4';
+})
+
+document.querySelector('#btn_5').addEventListener('click', function () {
+    inputWindow.value += '5';
+})
+
+document.querySelector('#btn_6').addEventListener('click', function () {
+    inputWindow.value += '6';
+})
+
+document.querySelector('#btn_7').addEventListener('click', function () {
+    inputWindow.value += '7';
+})
+
+document.querySelector('#btn_8').addEventListener('click', function () {
+    inputWindow.value += '8';
+})
+
+document.querySelector('#btn_9').addEventListener('click', function () {
+    inputWindow.value += '9';
+})
+
+document.querySelector('#btn_0').addEventListener('click', function () {
+    inputWindow.value += '0';
+})
--- bjs/08_if_else/script.js
+++ bjs/08_if_else/script.js
@@ -1,23 +1,57 @@
-let minValue = parseInt(prompt('Минимальное знание числа для игры','0'));
-let maxValue = parseInt(prompt('Максимальное знание числа для игры','100'));
+while (true) {
+        minValue = parseInt(prompt('Минимальное знание числа для игры','-999'));
+    
+    if (minValue < -999) {
+         alert('Число должно быть не меньше -999');
+        continue;
+    }
+    
+    break;
+}
+
+while (true) {
+    maxValue = parseInt(prompt('Максимальное знание числа для игры','999'));
+
+    if (maxValue > 999) {
+        alert('Число должно быть меньше 1000');
+        continue;
+    }
+
+    if (maxValue < minValue) {
+        alert('Число должно быть больше минимального значения');
+        continue;
+    }
+
+    break;
+}
+
 alert(`Загадайте любое целое число от ${minValue} до ${maxValue}, а я его угадаю`);
+
 let answerNumber  = Math.floor((minValue + maxValue) / 2);
 let orderNumber = 1;
 let gameRun = true;
 
-const orderNumberField = document.getElementById('orderNumberField');
-const answerField = document.getElementById('answerField');
+let answers = [`Я всегда угадываю!\n\u{1F60E}` , `Уххх!\n\u{1F60E}` , `Да это легко!\n\u{1F60E}`];
+let randomAnswers = Math.floor(Math.random() * (answers.length - 1));
+
+
+const orderNumberField = document.querySelector('#orderNumberField');
+const answerField = document.querySelector('#answerField');
 
 orderNumberField.innerText = orderNumber;
-answerField.innerText = `Вы загадали число ${answerNumber }?`;
 
-document.getElementById('btnRetry').addEventListener('click', function () {
-    minValue = 0;
-    maxValue = 100;
-    orderNumber = 0;
-})
 
-document.getElementById('btnOver').addEventListener('click', function () {
+function randomQuestionUpdate () {
+    let question = [`Вы загадали число ${answerNumber}?` , `Угадал ${answerNumber}?` , `Это число ${answerNumber}?`];
+    let randomQuestion = Math.floor(Math.random() * (question.length));
+
+    answerField.innerText = question[randomQuestion];
+}
+
+randomQuestionUpdate();
+
+
+document.querySelector('#btnOver').addEventListener('click', function () {
     if (gameRun){
         if (minValue === maxValue){
             const phraseRandom = Math.round( Math.random());
@@ -28,19 +62,43 @@ document.getElementById('btnOver').addEventListener('click', function () {
             answerField.innerText = answerPhrase;
             gameRun = false;
         } else {
-            minValue = answerNumber  + 1;
+            minValue = answerNumber;
             answerNumber  = Math.floor((minValue + maxValue) / 2);
-            orderNumber++;
+            orderNumber++;  
             orderNumberField.innerText = orderNumber;
-            answerField.innerText = `Вы загадали число ${answerNumber }?`;
+            randomQuestionUpdate();
         }
     }
 })
 
-document.getElementById('btnEqual').addEventListener('click', function () {
+document.querySelector('#btnLess').addEventListener('click', function () {
     if (gameRun){
-        answerField.innerText = `Я всегда угадываю\n\u{1F60E}`
+        if (minValue === maxValue){
+            const phraseRandom = Math.round( Math.random());
+            const answerPhrase = (phraseRandom === 1) ?
+                `Вы загадали неправильное число!\n\u{1F914}` :
+                `Я сдаюсь..\n\u{1F92F}`;
+
+            answerField.innerText = answerPhrase;
+            gameRun = false;
+        } else {
+            maxValue = answerNumber;
+            answerNumber  = Math.floor((minValue + maxValue) / 2);
+            orderNumber++;
+            orderNumberField.innerText = orderNumber;
+            randomQuestionUpdate();
+        }
+    }
+})
+
+document.querySelector('#btnEqual').addEventListener('click', function () {
+    if (gameRun){
+        answerField.innerText = answers[randomAnswers];
         gameRun = false;
     }
 })
 
+document.querySelector(`#btnRetry`).addEventListener(`click`, function () {
+    location.reload()
+    alert("Да начнется веселье!")
+})
